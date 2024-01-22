1b8ab6c55a7f386b2f74df318b40f137761dd7e4
Martinnord <martin.nordstrom99@gmail.com>
2024-01-22 12:13:29 +0100

    Added usage of paper

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,19 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import * as paper from 'paper';
+
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-17-test';
+
+  private test(): paper.Color {
+    return new paper.Color(0, 0, 0);
+  }
 }
